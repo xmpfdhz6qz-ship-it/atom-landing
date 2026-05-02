c1acb3211163b04f9f599341da9ef7b2c7aca13f
xmpfdhz6qz-ship-it <daniel.pokorny3@gmail.com>
2026-05-02 16:25:20 +0200

    fix: accept slug or domain param

--- api/score.js
+++ api/score.js
@@ -6,10 +6,7 @@ const pool = new Pool({
     });
 
     export default async function handler(req, res) {
-      const { domain } = req.query;
-
-        if (!domain) {
-            return res.status(400).json({ error: 'Missing domain' });
+  const domain = req.query.domain || req.query.slug;
               }
 
                 try {
